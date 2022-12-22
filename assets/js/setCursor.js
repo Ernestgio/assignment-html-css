03adee5a036e3e905f1da6f33333e303a9e7cbbc
Ernest Giovanni <giovanni.ernest@shopee.com>
2022-12-22 22:47:16 +0700

    added contact us page

---
+++ assets/js/setCursor.js
@@ -0,0 +1,8 @@
+function setCursor() {
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("subject").value = "";
+  document.getElementById("message").value = "";
+
+  document.getElementById("name").focus();
+}
